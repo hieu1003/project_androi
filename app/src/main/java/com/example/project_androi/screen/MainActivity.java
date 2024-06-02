e5b1b0a920591769cafea54642425b5095c23040
tienha <160502668+hanhattien2003@users.noreply.github.com>
2024-06-02 23:43:46 +0700

    tien cmt

--- app/src/main/java/com/example/project_androi/screen/MainActivity.java
+++ app/src/main/java/com/example/project_androi/screen/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.project_androi;
+package com.example.project_androi.screen;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project_androi.R;
 
 public class MainActivity extends AppCompatActivity {
 
